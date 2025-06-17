8aa3445375cd9148fe53c75ff1757024f1993dae
Zhou <zhoukashen@gmail.com>
2025-06-17 01:44:36 -0300

    Aula de JavaScript

--- Aula05/js/script.js
+++ Aula05/js/script.js
@@ -1,4 +1,5 @@
 /*
+Uma função JavaScript é um bloco de código projetado para executar uma tarefa específica.
 
 É como uma pequena "fábrica" onde você faz uma entrada e ele te dá uma saída.
 
@@ -8,39 +9,36 @@ que vai constribuir para todo código.
 Uma função JavaScript é executada quando "algo" a invoca (chama-a).
 */
 
-function soma(valor1, valor2) {
-    return valor1 + valor2;
-}
+// // Função de soma
+// function soma (valor1, valor2) {
+//     return valor1 + valor2;
+// }
 
-// Função da cotação do dólar
-function realParaDolar(real, cotacaoDolar) {
-    return real * cotacaoDolar;
-}
+// // Função de cotação do dólar
+// function realParaDolar(real, cotacaoDolar) {
+//     return real * cotacaoDolar;
+// }
+
+// var valorReal = 7.89;
+// var cotacao = 5.08;
+
+// // document.getElementById("texto").innerHTML = soma(10, 23);
+
+// var total = realParaDolar(valorReal, cotacao);
+
+// alert("O valor em real é R$: " + valorReal + " o valor em dólar U$ é: " + total);
 
 function alertaHello() {
-    alert("Olá pessoal")
+     alert("Olá pessoal");
 }
 
 function paraCelsius(valorFahrenheit) {
     return (5/9) * (valorFahrenheit - 32);
-
 }
-
-alertaHello();
-
-var valorReal = 7.89;
-var cotacao = 5.08;
-
-var total = realParaDolar(valorReal, cotacao);
-
-function teste() {
-    var x = 10;
-    console.log(x)
-}
-
-teste()
-
-alert("O valor em real é R$: " + valorReal + " o valor em dólar é $: " + parseFloat(total.toFixed(2)));
 
 var x = paraCelsius(77);
 alert("A temperatura é de " + x + " graus celsius");
+
+function minhaFuncao() {
+    var x = 2;
+}
